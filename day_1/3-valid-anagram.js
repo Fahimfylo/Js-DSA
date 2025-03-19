cfebb8eed2e9e6846496addc07b65599e5c56acd
ghost <ghostpy91.com>
2025-03-19 01:45:55 -0700

    solved Array problems

---
+++ day_1/3-valid-anagram.js
@@ -0,0 +1,35 @@
+// Ques 3 - Valid Anagram
+// An Anagram is a word or phase formated rearranging the letters of
+// a different word or phase , using all the original letters exactly once
+
+// Input: (s = 'anagram'), (t = 'nagaram') ----> Output: true
+// Input: (s = 'rat'), (t = 'car') ----> Output: true
+
+// 1st Approach
+const isAnagram1 = function (i, j) {
+  i = i.split("").sort().join("");
+  j = j.split("").sort().join("");
+
+  return i === j;
+};
+
+// cinema => [c,i,n,e,m,a] => [a, c, e, i, m, n] => cinema
+// iceman => [i,c,e,m,a,n] => [a, c, e, i, m, n] => iceman
+
+// 2nd Arrrocah
+const isAnagram2 = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  let Obj1 = {};
+  let Obj2 = {};
+
+  for (let i = 0; i < s.length; i++) {
+    Obj1[s[i]] = (Obj1[s[i]] || 0) + 1;
+    Obj2[t[i]] = (Obj2[t[i]] || 0) + 1;
+  }
+
+  for (const key in Obj1) {
+    if (Obj1[key] !== Obj2[key]) return false;
+  }
+  return true;
+};
