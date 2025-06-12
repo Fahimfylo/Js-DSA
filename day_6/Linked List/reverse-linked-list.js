8392efacb62682bd0247acf56ab968d2d7aaa282
ghost <ghostpy91.com>
2025-06-12 16:47:50 +0600

    reverse lists

--- day_6/Linked List/reverse-linked-list.js
+++ day_6/Linked List/reverse-linked-list.js
@@ -6,7 +6,6 @@
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
- * }
  */
 var reverseList = (head) => {
   let prev = null;
